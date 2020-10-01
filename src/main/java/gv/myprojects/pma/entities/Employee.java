e282916890675cf1fdf5999a77261b2d77144233
Genesis Villalvazo <genesisvillalvazo24@gmail.com>
2020-09-30 19:17:54 -0500

    Created Employee POJO. EmployeeId will be autogenerated.

--- src/main/java/gv/myprojects/pma/entities/Employee.java
+++ src/main/java/gv/myprojects/pma/entities/Employee.java
@@ -1,5 +1,12 @@
 package gv.myprojects.pma.entities;
 
 public class Employee {
+	
+	private long employeeId;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	
 
 }
